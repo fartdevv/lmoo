17143df3c875927a926f9d5ad99d2f14f0d50e86
sean <0xF0CACC1A@proton.me>
2025-07-29 10:10:12 +0700

    Update lmodroid_merlinx.mk

--- lmodroid_merlinx.mk
+++ lmodroid_merlinx.mk
@@ -10,7 +10,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from device makefile.
 $(call inherit-product, device/xiaomi/merlinx/device.mk)
-$(call inherit-product, vendor/lmodroid-priv/keys.mk)
+$(call inherit-product, vendor/lmodroid-priv/keys/keys.mk)
 
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lmodroid/config/common_full_phone.mk)
